47a5df1cf8536e982253692ea030f71ac0a019e3
Rijan Timsina <regiontimsina@gmail.com>
2020-12-30 13:30:32 -0500

    changes seconds to minutes

--- battery.py
+++ battery.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
         if battery_percent == 100 and battery.power_plugged:
             end = time.time()
             DiscordWebhook(url="https://discord.com/api/webhooks/793655410749997077/UWaKE9FyLnu8zqW1GOwR7uzHgQGp_Ycm-diAaZvLuPNo0zK1ysmULNwRehNPv2Sd0Xmb",
-                           content="Battery Full, Please unplug\nTime Taken = {}s".format(int(end - start)/60)).execute()
+                           content="Battery Full, Please unplug\nTime Taken = {} minutes".format(int(end - start)/60)).execute()
             quit()
         time.sleep(60)
