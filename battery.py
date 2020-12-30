9b752f28695bc5d3c581fb97f22077b2d48d7e7f
Rijan Timsina <regiontimsina@gmail.com>
2020-12-30 13:31:49 -0500

    documentation

--- battery.py
+++ battery.py
@@ -2,6 +2,10 @@ from discord_webhook import DiscordWebhook
 import psutil as psu
 import time
 
+# your discord webhook url goes here
+WEBHOOK_URL = "https://discord.com/api/webhooks/793655410749997077/UWaKE9FyLnu8zqW1GOwR7uzHgQGp_Ycm-diAaZvLuPNo0zK1ysmULNwRehNPv2Sd0Xmb"
+
+
 if __name__ == "__main__":
     start = time.time()
     print("Battery Monitor Started")
@@ -10,7 +14,7 @@ if __name__ == "__main__":
         battery_percent = int(str(battery.percent))
         if battery_percent == 100 and battery.power_plugged:
             end = time.time()
-            DiscordWebhook(url="https://discord.com/api/webhooks/793655410749997077/UWaKE9FyLnu8zqW1GOwR7uzHgQGp_Ycm-diAaZvLuPNo0zK1ysmULNwRehNPv2Sd0Xmb",
-                           content="Battery Full, Please unplug\nTime Taken = {} minutes".format(int(end - start)/60)).execute()
+            DiscordWebhook(url=WEBHOOK_URL, content="Battery Full, Please unplug\nTime Taken = {} minutes".format(
+                int(end - start)/60)).execute()
             quit()
         time.sleep(60)
